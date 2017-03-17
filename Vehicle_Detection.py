2aa72e289a29ad7bbcaa455907a1a7aa6abfefcd
Eddy Chu <shuk0902@gmail.com>
2017-03-17 11:33:52 -0700

    Updated HOG parameter after first rerview

--- Vehicle_Detection.py
+++ Vehicle_Detection.py
@@ -334,11 +334,11 @@ notcars = notcars
 
 ### TODO: Tweak these parameters and see how the results change.
 color_space = 'YCrCb' # Can be RGB, HSV, LUV++, HLS, YUV+, YCrCb+++
-orient = 9  # HOG orientations
-pix_per_cell = 8 # HOG pixels per cell
+orient = 11  # HOG orientations
+pix_per_cell = 16 # HOG pixels per cell
 cell_per_block = 2 # HOG cells per block
 hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
-spatial_size = (32, 32) # Spatial binning dimensions
+spatial_size = (16, 16) # Spatial binning dimensions
 hist_bins = 32    # Number of histogram bins
 spatial_feat = True # Spatial features on or off
 hist_feat = True # Histogram features on or off
